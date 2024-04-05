afa72cc1e09ef49a7d6b8fe3809492852a9d1f55
flyingkitties <110839612+flyingkitties@users.noreply.github.com>
2024-04-05 08:21:53 +0100

    Pricing section + type toggle

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Header from '@/components/Header';
 import Hero from '@/components/Hero';
+import Pricing from '@/components/pricing/Pricing';
 import Image from 'next/image';
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <main className="px-5 md:px-[10%] xl:px-[15%]">
       <Header />
       <Hero />
+      <Pricing />
     </main>
   );
 }
